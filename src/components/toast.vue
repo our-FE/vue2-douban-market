da4e3d407ed2505e0a93a8d8c93aa060db771000
gzzhangyingying <gzzhangyingying@corp.netease.com>
2017-10-11 11:02:24 +0800

    购物车

---
+++ src/components/toast.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="toast">{{message}}</div>
+</template>
+<script type="text/javascript">
+
+
+  export default {
+    name:'toast',
+    props:['message']
+  }
+</script>
+<style scoped>
+  .toast{
+    background: #000;
+    opacity: 0.4;
+    padding: 5px 10px;
+    text-align: center;
+    color: #fff;
+    font-size: 12px;
+  }
+</style>
