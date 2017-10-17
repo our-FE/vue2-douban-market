77cc96de32bf4e977897027116a9bed0554fd435
gzzhangyingying <gzzhangyingying@corp.netease.com>
2017-10-17 19:50:10 +0800

    购物车打钩

--- src/pages/cart/cart.vue
+++ src/pages/cart/cart.vue
@@ -2,7 +2,30 @@
   <div class="cart">
     <header-bar></header-bar>
     <nav-bar></nav-bar>
-    <good :cart="cart"></good>
+    <div class="buyCar">
+      <ul>
+        <li v-for="(item,index) in cart">
+          <good :item="item" :index="index" :chooseBuy="chooseBuy"></good>
+        </li>
+      </ul>
+    </div>
+    <div class="footer">
+      <div class="f-fx">
+        <div class="ft-cb">
+          <input id="cb-footer" type="checkbox" class="input-cb">
+          <label for="cb-footer" class="footer-cb" ></label>
+        </div>
+        <div class="qx">全选</div>
+        <div class="pay">
+          <span class="hj">合计：</span>
+          <span class="money">￥0.0</span>
+        </div>
+        <div class="btn">
+          <span>结算</span>
+          <span>(</span><span>0</span><span>)</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,7 +34,7 @@
   import HeaderBar from '../../components/header.vue'
   import NavBar from '../../components/nav.vue'
   import Good from './good.vue'
-
+  import vue from 'vue'
 
   export default {
     computed: {
@@ -25,7 +48,20 @@
       this.getCart();
     },
     methods: {
-      ...mapActions(['getCart'])
+      ...mapActions(['getCart']),
+      chooseBuy: function (item,index,that) {
+        if (item) {
+          let checked = item.checked;
+          // 勾选
+
+          item.checked = checked === '1' ? '0' : '1';
+          this.cart[index] = item;
+          //this.$set('cart['+index+']','checked',checked);
+          vue.set(this.cart,this.cart);
+        } else {
+          console.log('缺少所需参数')
+        }
+      }
     },
     components: {
       HeaderBar,
@@ -34,47 +70,64 @@
     }
   }
 </script>
-<style scoped>
-  .login_false .login_false{
-    text-align: center;
-    padding: 1rem 0;
-    border-bottom: 1px solid #9d9d9d;
-    background-color: #f8f8fe;
-  }
-  .login_false .login_entry{
-    padding: 20px;
-    text-align: center;
-    border-bottom: 1px solid #9d9d9d;
-    background-color: #fff;
-  }
-  .login_false .login_entry .login_tip2{
-    color: #4a4a4a;
-  }
-  .login_false .login_entry .btn_login{
-    display: block;
-    padding: 10px 0;
-    text-align: center;
-    margin: 15px 0;
-    color: #fff;
-    font-size: 14px;
-    border-radius: 5px;
-    background-color: #68cb78;
-  }
-  .login_false .login_entry .btn_regist{
-    color: #9d9d9d;
-  }
-  .login_false .login_entry .btn_regist a{
-    color: #68cb78;
-  }
-  .login_false .anonymous{
-    text-align: center;
-    color: #9b9b9b;
-    padding-top: 2rem;
-  }
-  .login_false .anonymous p{
-    padding: 5px 0;
-  }
-  .login_false .anonymous a{
-    color: #68cb78;
+<style scoped lang="scss">
+  .footer {
+    cursor: pointer;
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border-top: .8rem solid transparent;
+    display: flex;
+
+    .input-cb {
+      /*display: none;*/
+    }
+
+    .footer-cb {
+      display: inline-block;
+      width: 0.533rem;
+      height: 0.533rem;
+      cursor: pointer;
+      background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoBAMAAAB+0KVeAAAAA3NCSVQICAjb4U/gAAAALVBMVEX///+hprehprehprehprehprehprehprehprehprehprehprehprehprehprd0XKVXAAAAD3RSTlMAESIzRGZ3iJmqu8zd7v8zDtSdAAAACXBIWXMAAAsSAAALEgHS3X78AAAAHHRFWHRTb2Z0d2FyZQBBZG9iZSBGaXJld29ya3MgQ1M26LyyjAAAAPVJREFUKJF1k88KQUEUxg/yrygLG4m8gLKVUlZ28gTyAsrCTskLKLL3BFKysJO1IixkYSMkrr5nMPeie+/0OYuZ6TedP3PONyKWecpjYNWKicO8PVh2zDjYSIHTWi0PmzaAeUkk3gYOP5YAhp9THhh8kyww+90XYHySJXG3w3cwtfYRqjb042FuAdyc1dVQV2sWTScMYm96G+KyhfL34uKGRWQkhL4bhtGVFHJu6MNOKlpIFfQqnbsOFZlcdVh5yuasw/RLsNVhFIKlDiP/IHOniWhJtHj6TNoQ2jraZDoOPjg6YioGLhsqMCpFLloub/oR9C/zBsU0n4PhpWahAAAAAElFTkSuQmCC) no-repeat 0 0;
+      background-size: 0.52rem;
+    }
+
+    .f-fx {
+      width: 100%;
+      height: 50px;
+      background: #fff;
+      border-top: 1px solid #e7e7e7;
+      display: flex;
+      line-height: 50px;
+
+      .qx {
+        flex: 1;
+        font-size: 15px;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+      }
+
+      .pay {
+        flex: 2;
+        align-items: center;
+        text-align: right;
+        padding: 0 10px;
+
+        .money {
+          color: #e17c72;
+          font-size: 18px;
+        }
+
+      }
+      .btn {
+        flex: 1;
+        text-align: center;
+        color: #fff;
+        background: #e17c72;
+      }
+
+    }
   }
 </style>
--- src/pages/cart/good.vue
+++ src/pages/cart/good.vue
@@ -1,48 +1,43 @@
 <template>
-  <div class="buyCar">
-    <ul>
-      <li v-for="(product,index) in cart" class="car_item">
-        <!--<div class="car_item">-->
-        <div class="car_choose"><p><input :id="index" type="checkbox" class="input-box"><label :for="index"></label></p>
-        </div>
-        <a :href="product.url+'/'+product.id" class="car_img">
-          <img :src="product.src" height="300" width="300">
-        </a>
-        <div class="car_detail">
-          <h3 class="pdt_title">
-            <a :href="product.url">{{product.title}}</a>
-          </h3>
-          <p class="car_price">
-            <span class="pdt_new_price">{{product.newPrice}}</span>
-            <del class="pdt_old_price">{{product.oldPrice}}</del>
-          </p>
-        </div>
-        <!--</div>-->
-      </li>
-    </ul>
-    <div class="footer">
-      <div class="f-fx">
-          <div class="ft-cb">
-            <input id="cb-footer" type="checkbox" class="input-cb">
-            <label for="cb-footer" class="footer-cb"></label>
-          </div>
-          <div class="qx" >全选</div>
-          <div class="pay">
-              <span class="hj">合计：</span>
-              <span class="money">￥0.0</span>
-          </div>
-          <div class="btn" >
-            <span>结算</span>
-            <span>(</span><span>0</span><span>)</span>
-          </div>
-        </div>
+  <div class="car_item">
+    <div class="car_choose">
+      <!--<p>-->
+        <input :id="index" type="checkbox" class="input-box" >
+        <label :for="index" :class="{'checkbox-on':item.checked === '1'}"  @click="choose(item,index)"></label>
+      <!--</p>-->
+    </div>
+    <a :href="item.url+'/'+item.id" class="car_img">
+      <img :src="item.src" height="300" width="300">
+    </a>
+    <div class="car_detail">
+      <h3 class="pdt_title">
+        <a :href="item.url">{{item.title}}</a>
+      </h3>
+      <p class="car_price">
+        <span class="pdt_new_price">{{item.newPrice}}</span>
+        <del class="pdt_old_price">{{item.oldPrice}}</del>
+      </p>
     </div>
   </div>
+
 </template>
 <script type="text/javascript">
+//  let data = {
+//    isChecked:false
+//  };
   export default {
     name: 'good',
-    props: ['cart']
+    props: ['item','index','chooseBuy'],
+//    data() {
+//      return data
+//    },
+    methods: {
+      choose: function(item,index){
+//        data.isChecked = !data.isChecked;
+        this.chooseBuy(item,index,this);
+      }
+    }
+
   }
 </script>
 <style lang="scss">
@@ -60,6 +55,14 @@
         height: 0.533rem;
         cursor: pointer;
         background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoBAMAAAB+0KVeAAAAA3NCSVQICAjb4U/gAAAALVBMVEX///+hprehprehprehprehprehprehprehprehprehprehprehprehprehprd0XKVXAAAAD3RSTlMAESIzRGZ3iJmqu8zd7v8zDtSdAAAACXBIWXMAAAsSAAALEgHS3X78AAAAHHRFWHRTb2Z0d2FyZQBBZG9iZSBGaXJld29ya3MgQ1M26LyyjAAAAPVJREFUKJF1k88KQUEUxg/yrygLG4m8gLKVUlZ28gTyAsrCTskLKLL3BFKysJO1IixkYSMkrr5nMPeie+/0OYuZ6TedP3PONyKWecpjYNWKicO8PVh2zDjYSIHTWi0PmzaAeUkk3gYOP5YAhp9THhh8kyww+90XYHySJXG3w3cwtfYRqjb042FuAdyc1dVQV2sWTScMYm96G+KyhfL34uKGRWQkhL4bhtGVFHJu6MNOKlpIFfQqnbsOFZlcdVh5yuasw/RLsNVhFIKlDiP/IHOniWhJtHj6TNoQ2jraZDoOPjg6YioGLhsqMCpFLloub/oR9C/zBsU0n4PhpWahAAAAAElFTkSuQmCC) no-repeat 0 0;
+        background-size: 0.52rem;
+      }
+      label.checkbox-on{
+        display: inline-block;
+        width: 0.533rem;
+        height: 0.533rem;
+        cursor: pointer;
+        background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAMAAAC7IEhfAAAAA3NCSVQICAjb4U/gAAAAh1BMVEX/////UAD/UAD/UAD/UAD/UAD/UAD/UAD/UAD/UAD/UAD/UAD/////+vj/9fD/8ev/8On/7OP/6d//5Nj/4NL/3s//2cj/1MH/zLX/w6f/wKP/upv/t5b/rYf/qID/oXb/mWb/jlr/jFj/dTb/cC//biz/bSr/aCP/XhT/Wg7/Vgn/UQL/UADQOR1YAAAALXRSTlMAESJEd4iZqrvM3e7///////////////////////////////////////////9INcYgAAAACXBIWXMAAAsSAAALEgHS3X78AAAAHHRFWHRTb2Z0d2FyZQBBZG9iZSBGaXJld29ya3MgQ1M26LyyjAAAARJJREFUOI2N1et2wiAMAOBApVZg7j7dzbn71Lz/8w0qcsKtJr/EfgcIDSkADdGpQaMLPahOQCtkb5CE6WWdzbGIeYXOTOncrLOMicp0YdJkq2LRcogLwXOJbK4bVo95TDvEkJGs5kvDSM7CcXF53iH6KXsO7N3RnN2hDyOgaz/9evyIvztQTfe5tBeH00DB0HLfl9au42gAPeFW+zjUMOEeduSPBiwchdvbbdtReGNtkD+lQ5LMmw3y97p0mh7Pq5PvR3efOXc89MBH6d3dX56dSl/hi5NXNedeYVoUXtacK4qszJ6qzpdZVriHzXPFjYXLvgr8y8W+rvwGwG8p7CbFb3v8Rgr81gzsZj9ulX4+0kf/qVXH8bP2GwMAAAAASUVORK5CYII=) no-repeat 0 0;
         background-size: 0.52rem;
       }
     }
@@ -88,58 +91,6 @@
           &:first-letter {
             font-size: 12px;
           }
-        }
-      }
-    }
-    .footer{
-      cursor: pointer;
-      position: fixed;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      border-top: .8rem solid transparent;
-      display: flex;
-      .input-cb{
-        /*display: none;*/
-      }
-      .footer-cb{
-        display: inline-block;
-        width: 0.533rem;
-        height: 0.533rem;
-        cursor: pointer;
-        background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoBAMAAAB+0KVeAAAAA3NCSVQICAjb4U/gAAAALVBMVEX///+hprehprehprehprehprehprehprehprehprehprehprehprehprehprd0XKVXAAAAD3RSTlMAESIzRGZ3iJmqu8zd7v8zDtSdAAAACXBIWXMAAAsSAAALEgHS3X78AAAAHHRFWHRTb2Z0d2FyZQBBZG9iZSBGaXJld29ya3MgQ1M26LyyjAAAAPVJREFUKJF1k88KQUEUxg/yrygLG4m8gLKVUlZ28gTyAsrCTskLKLL3BFKysJO1IixkYSMkrr5nMPeie+/0OYuZ6TedP3PONyKWecpjYNWKicO8PVh2zDjYSIHTWi0PmzaAeUkk3gYOP5YAhp9THhh8kyww+90XYHySJXG3w3cwtfYRqjb042FuAdyc1dVQV2sWTScMYm96G+KyhfL34uKGRWQkhL4bhtGVFHJu6MNOKlpIFfQqnbsOFZlcdVh5yuasw/RLsNVhFIKlDiP/IHOniWhJtHj6TNoQ2jraZDoOPjg6YioGLhsqMCpFLloub/oR9C/zBsU0n4PhpWahAAAAAElFTkSuQmCC) no-repeat 0 0;
-        background-size: 0.52rem;
-      }
-      .f-fx{
-        width: 100%;
-        height: 50px;
-        background: #fff;
-        border-top: 1px solid #e7e7e7;
-        display: flex;
-        line-height:50px;
-        .qx{
-          flex: 1;
-          font-size: 15px;
-          justify-content: center;
-          align-items: center;
-          text-align: center;
-        }
-        .pay{
-          flex: 2;
-          align-items: center;
-          text-align: right;
-          padding: 0 10px;
-          .money{
-            color: #e17c72;
-            font-size: 18px;
-          }
-
-        }
-        .btn{
-          flex: 1;
-          text-align: center;
-          color: #fff;
-          background: #e17c72;
         }
       }
     }
