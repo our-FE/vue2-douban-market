55f182289ad69a40f9603908508e35eb67407efe
gzzhangyingying <gzzhangyingying@corp.netease.com>
2017-10-18 09:21:00 +0800

    购物车+

--- src/store/actions.js
+++ src/store/actions.js
@@ -57,6 +57,21 @@ export default {
 			}
 		})
 	},
+  getCart({commit,state}){
+    state.busy = true;
+    commit(types.IS_SHOW_LOADING_TIPS,true);
+    axios.get('/mock/cart/cart.json').then((response)=>{
+      commit(types.IS_SHOW_LOADING_TIPS,false);
+      let result = response.data.list.slice(state.num-5,state.num);
+      if(result.length !== 0){
+        commit(types.GET_CART,result);
+        state.busy = false;
+        state.num+=5;
+      }else{
+        commit(types.IS_SHOW_LOADED_TIPS,true);
+      }
+    })
+  }
 
 }
 
